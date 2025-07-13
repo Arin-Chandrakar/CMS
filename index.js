2c0b242af6fec017ed91a77c13ddbf881f18104f
Arin-Chandrakar <arin23official@gmail.com>
2025-07-13 11:48:33 +0530

    cms system beginning

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const JWT=require("jsonwebtoken");
+const mongoose=require("mongoose");
+const app=express();
+
+
+
+app.get("/",function(req,res){
+
+})
+
+app.post("/user/signup",function(req,res){
+    
+})
+
+app.post("/user/signin",function(req,res){
+    
+})
+
+app.post("/courses",function(req,res){
+    
+})
+
+app.post("/user/purchases",function(req,res){
+    
+})
+
+app.listen(5000);
